b6fad09543c48f34d3a1b2572e5c8bd64e5b5d5b
fahad-accordian-23 <fahad.1979.hassan@gmail.com>
2024-05-30 03:01:58 +0500

    Structure for Zombie class

--- Game/Zombies.h
+++ Game/Zombies.h
@@ -4,20 +4,37 @@
 /* Forward Declarations
  */
 class Animation;
+class Plant;
 
 class Zombie
 {
 protected:
-    Animation** animations;
-    float health;
+    Animation* animations;
+    float x;
+    float y;
     float step;
-    int state;
+    float deltaTime;
+    int health;
+    int damage;
+    int currentAnimation;
+    char state;
 
 public:
+    Zombie(Animation* animations, float x, float y, float step);
     virtual void move() = 0;
-    virtual void die() = 0;
-    virtual bool takeDamage(float damage) = 0;
-    virtual void animate() = 0;
+    virtual bool animate() = 0;
+    virtual void attack(Plant* plant) = 0;
+    virtual void takeDamage(float damage) = 0;
+};
+
+class NormalZombie : public Zombie
+{
+public:
+    NormalZombie(Animation* animations, float x, float y, float step);
+    void move() override;
+    bool animate() override;
+    void attack(Plant* plant) override;
+    void takeDamage(float damage) override;
 };
 
 #endif
